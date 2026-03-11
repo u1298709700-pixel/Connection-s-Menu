aefaa4503514dc067c8bab511c49bbcb98854245
u1298709700-pixel <u1298709700@gmail.com>
2026-03-11 15:54:41 +0100

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,9 @@
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+Rayfield:CreateWindow({
+	Name = "Connection's FTAP Menu";
+	Icon = 12117740;
+	LoadingTitle = "Connection's FTAP Menu";
+	LoadingSubtitle = "By Endermari123isthaun";
+	Theme = "DarkBlue";
+});
